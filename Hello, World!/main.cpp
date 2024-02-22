04338bfb1203662e9621f4d7c759d3b31feba6f9
Blame-Lazarus <154845784+Blame-Lazarus@users.noreply.github.com>
2024-02-22 12:16:52 -0800

    Add files via upload

---
+++ Hello, World!/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: lazarusx
+ *
+ * Created on February 22, 2024, 11:03 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(void) {
+
+    cout << "Hello, World!\n";
+    return 0;
+}
+
